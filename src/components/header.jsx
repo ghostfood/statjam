87a77c82a363196d73ccab1cd2e4c3943c6eb3a2
ghostfood <ghostfoodtv@gmail.com>
2026-08-29 19:20:04 +0100

    debug and update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,27 +2,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    // style={{
-    //   background: `rebeccapurple`,
-    //   marginBottom: `1.45rem`,
-    // }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <a
-          href="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1>
+        <a href="/">
           {siteTitle}
         </a>
       </h1>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,11 +7,23 @@ import "./layout.css"
 const Layout = ({ children, siteTitle = "Statjam" }) => {
   return (
     <>
-      <Header siteTitle={siteTitle} />
-        <main>{children}</main>
-        <footer class="d-flex justify-content-center align-items-center">
-          © {new Date().getFullYear()}, Built with Astro
-        </footer>
+    <Header siteTitle={siteTitle} />
+  <main>{children}</main>
+    <footer className="d-flex flex-column justify-content-center align-items-center">
+      <div className="d-flex flex-column w-100 container container-xl my-3">
+        <div className="row">
+          <div className="col-12 align-center">
+            <div className="d-flex justify-content-center">
+              <a href="http://ghostfood.uk" className="d-flex flex-column align-center align-inline" title="Go to ghostfood.uk">
+                <img src="/img/icon-ghostfood.png" height="40" width="40" className="swing d-flex align-center rounded-circle my-2 p-1" alt="Ghost Food logo" />
+                <div className="align-center small">A Ghost Food production</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      © {new Date().getFullYear()}, Built with Astro
+    </footer>
     </>
   )
 }
